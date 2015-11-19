c393869167ea85c0eae44d7a3eaa22102c601048
zoujiaqing <zoujq@putao.com>
2015-11-19 17:41:16 +0800

    use KERISY_ENV load .dev.php / .test.php / .php config files

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -18,6 +18,17 @@ class Config extends Set
 {
     public function __construct($config_group)
     {
-        $this->data = require CONFIG_PATH . $config_group . '.php';
+        $ext_name = '.php';
+        
+        $config_file = CONFIG_PATH . $config_group . $ext_name;
+        
+        /* 环境变量加载不同扩展名的配置文件 */
+        $env_ext_name = (KERISY_ENV == 'development' ? '.dev' : (KERISY_ENV == 'test' ? '.test' : '')) . '.php';
+        $env_config_file = CONFIG_PATH . $config_group . $env_ext_name;
+        
+        /* ENV配置文件不存在的情况下默认加载正式环境配置文件 */
+        $config_file = file_exists($env_config_file) ? $env_config_file : $config_file;
+        
+        $this->data = require $config_file;
     }
 }
