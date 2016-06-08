bfc00dcb59a59af813a712301413c3e8ddfd29a2
haoyanfei <haoyf@putao.com>
2016-06-08 18:59:33 +0800

    compatible code ago~

---
+++ src/Database/LaravelORM/Model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Kerisy Framework
+ *
+ * PHP Version 7
+ *
+ * @author          Jiaqing Zou <zoujiaqing@gmail.com>
+ * @copyright      (c) 2015 putao.com, Inc.
+ * @package         kerisy/framework
+ * @subpackage      Database
+ * @since           2015/11/11
+ * @version         2.0.0
+ */
+
+namespace Kerisy\Database\LaravelORM;
+
+use \Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+    protected $connection = 'default';
+
+    public function __construct(array $attributes = [])
+    {
+        DB::signton($this->connection);
+        parent::__construct($attributes);
+    }
+}
---
+++ src/Database/LaravelORM/PTCapsule.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haoyanfei<haoyf@putao.com>
+ * Date: 2015/11/29
+ * Time: 18:24
+ */
+
+namespace Kerisy\Database\LaravelORM;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class PTCapsule extends Capsule
+{
+
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array([static::connection(), $method], $parameters);
+    }
+
+}
