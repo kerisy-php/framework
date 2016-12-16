2933a3ad5d8496e61583507a370f3fa0fa33f415
汪开辉 <hpuwang@gmail.com>
2016-12-16 12:19:54 +0800

    Mod: route bug 修复

--- src/kerisy/Foundation/Bootstrap/Bootstrap.php
+++ src/kerisy/Foundation/Bootstrap/Bootstrap.php
@@ -98,6 +98,7 @@ class Bootstrap
 
             if($params instanceof Response){
                 $controller = new HttpController();
+                $controller->view->msg = $e->getMessage();
                 if($config){
                     $content = $controller->render($config);
                 }else{
--- src/kerisy/Support/Exception/Page404Exception.php
+++ src/kerisy/Support/Exception/Page404Exception.php
@@ -16,5 +16,8 @@ namespace Kerisy\Support\Exception;
 
 class Page404Exception extends \Exception
 {
-
+    public function __construct($message, $code=0, Exception $previous=null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
